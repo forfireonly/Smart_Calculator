379e3ec07b8eccaa7a2448a9ae9679e76e79c70f
annascott <workwithpurpose@yahoo.com>
2020-12-24 14:26:13 -0700

    5/8' g

--- Smart Calculator/task/src/calculator/Addition.java
+++ Smart Calculator/task/src/calculator/Addition.java
@@ -6,65 +6,78 @@ public class Addition {
     Scanner scn;
     int firstAddend;
     int secondAddend;
+    boolean input;
 
     Addition(Scanner scn) {
         this.scn = scn;
     }
 
+
     public void readAddends() {
         String stringAddends = scn.nextLine();
         int sum = 0;
         String[] arrayOfAddends = stringAddends.split(" ");
-        if (arrayOfAddends.length > 1) {
+        if (arrayOfAddends[0].matches("[+-]*\\d*(?!^$)")) {
             sum = Integer.parseInt(arrayOfAddends[0]);
         }
-        while (!stringAddends.equals("/exit")) {
-            if (stringAddends.contains(" ")) {
-                //String[] arrayOfAddends = stringAddends.split(" ");
-                /*for (String member : arrayOfAddends) {
-                    sum += Integer.parseInt(member);
-                }*/
-                for (int i = 1; i < arrayOfAddends.length; i += 2) {
-                    if (arrayOfAddends[i].contains("+")) {
-                        sum += Integer.parseInt(arrayOfAddends[i + 1]);
-                    } else {
-                        if (arrayOfAddends[i].length()%2 == 0) {
-                            sum += Integer.parseInt(arrayOfAddends[i + 1]);
-                        } else {
-                            sum = sum - Integer.parseInt(arrayOfAddends[i + 1]);
-                        }
+
+        while (true) {
+            if (stringAddends.matches("[+-]?\\d*(\\s[+|-]*\\s\\d*)*") ||
+                    stringAddends.equals("/help") ||
+                    stringAddends.equals("/exit") ||
+                    stringAddends.equals("")) {
+                if (stringAddends.equals("/exit")) {
+                    System.out.println("bye");
+                    System.exit(0);
+                }
+                if (stringAddends.equals("/help")) {
+                    System.out.println("Calculator");
+                    stringAddends = scn.nextLine();
+                    arrayOfAddends = stringAddends.split(" ");
+                    if (arrayOfAddends.length > 1 || stringAddends.matches("[+-]*\\d*(?!^$)")) {
+                        sum = Integer.parseInt(arrayOfAddends[0]);
                     }
                 }
+                if (stringAddends.equals("")) {
+                    stringAddends = scn.nextLine();
+                    arrayOfAddends = stringAddends.split(" ");
+                    if (arrayOfAddends.length > 1 || stringAddends.matches("[+-]*\\d*(?!^$)")) {
+                        sum = Integer.parseInt(arrayOfAddends[0]);
+                    }
+                }
+                if (stringAddends.matches("[+-]?\\d*(\\s[+|-]*\\s\\d*)*")) {
+                    for (int i = 1; i < arrayOfAddends.length; i += 2) {
+                        if (arrayOfAddends[i].contains("+")) {
+                            sum += Integer.parseInt(arrayOfAddends[i + 1]);
+                        } else {
+                            if (arrayOfAddends[i].length() % 2 == 0) {
+                                sum += Integer.parseInt(arrayOfAddends[i + 1]);
+                            } else {
+                                sum = sum - Integer.parseInt(arrayOfAddends[i + 1]);
+                            }
+                        }
+                    }
+                    System.out.println(sum);
+                    stringAddends = scn.nextLine();
+                    arrayOfAddends = stringAddends.split(" ");
+                    if (arrayOfAddends.length > 1 || stringAddends.matches("[+-]*\\d*(?!^$)")) {
+                        sum = Integer.parseInt(arrayOfAddends[0]);
+                    }
+                }
+            } else {
 
-            } else if (stringAddends.equals("")) {
+                if (stringAddends.matches("/[a-zA-Z]*")) {
+                    System.out.println("Unknown command");
+                } else {
+                    System.out.println("Invalid expression");
+                }
                 stringAddends = scn.nextLine();
                 arrayOfAddends = stringAddends.split(" ");
-                if (arrayOfAddends.length > 1) {
+                if (stringAddends.matches("[+-]*\\d*(?!^$)")) {
                     sum = Integer.parseInt(arrayOfAddends[0]);
                 }
-                continue;
-            } else if (stringAddends.equals("/help")) {
-                System.out.println("The program calculates the sum of numbers");
-            } else {
-                sum = Integer.parseInt(stringAddends);
-
             }
 
-            System.out.println(sum);
-            stringAddends = scn.nextLine();
-            arrayOfAddends = stringAddends.split(" ");
-            if (arrayOfAddends.length > 1) {
-                sum = Integer.parseInt(arrayOfAddends[0]);
-            }
         }
-        System.out.println("Bye!");
     }
-
-
-    public void add() {
-        readAddends();
-        System.out.println(firstAddend + secondAddend);
-    }
-
-
 }
